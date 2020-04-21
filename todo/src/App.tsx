f06d50b06e9dec6bac1023b2c091d16a1208b0f0
Wesley Rocha <wesleysr1997@gmail.com>
2020-04-21 19:26:21 -0300

    Delete todo

--- todo/src/App.tsx
+++ todo/src/App.tsx
@@ -1,29 +1,45 @@
 import React from 'react';
 import './App.css';
 
+type Todo = Readonly<{
+    id: number
+    description: string
+    done: boolean
+}>
+
 function App() {
+    const [todos, setTodos] = React.useState<Todo[]>([
+        {
+            id: 1,
+            description: "Walk the dog",
+            done: true,
+        },
+        {
+            id: 2,
+            description: "Call John Doe",
+            done: false,
+        }
+    ]);
+
+    function deleteTodo(id: number) {
+        setTodos(todos.filter(todo => todo.id !== id));
+    }
+
     return (
-        <div className="todo">
+        <div className="todoComponent">
             <h1 className="title">Task list</h1>
             <div className="taskList">
-                <div className="task">
-                    <input type="checkbox"/>
-                    <span className="description">Buy groceries</span>
-                    <button className="delete">x</button>
-                </div>
-                <div className="task">
-                    <input type="checkbox"/>
-                    <span className="description">Walk the dog</span>
-                    <button className="delete">x</button>
-                </div>
-                <div className="task">
-                    <input type="checkbox"/>
-                    <span className="description">Call John Doe</span>
-                    <button className="delete">x</button>
-                </div>
+                {!todos.length && <h1>All tasks completed!</h1>}
+                {todos.map(todo => (
+                    <div className="todo">
+                        <input type="checkbox"/>
+                        <span className="description">{todo.description}</span>
+                        <button className="delete" onClick={() => deleteTodo(todo.id)}>x</button>
+                    </div>
+                ))}
             </div>
             <div className="newTask">
-                <input type="text"/>
+                <input type="text" />
                 <button className="create">+</button>
             </div>
         </div>
