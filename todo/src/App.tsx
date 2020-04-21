661f0ac33b12c1917e944bc37c14238ee393f4fe
Wesley Rocha <wesleysr1997@gmail.com>
2020-04-21 19:09:22 -0300

    Create HTML skeleton

--- todo/src/App.tsx
+++ todo/src/App.tsx
@@ -2,7 +2,32 @@ import React from 'react';
 import './App.css';
 
 function App() {
-    return <h1>iai boy</h1>;
+    return (
+        <div className="todo">
+            <h1 className="title">Task list</h1>
+            <div className="taskList">
+                <div className="task">
+                    <input type="checkbox"/>
+                    <span className="description">Buy groceries</span>
+                    <button className="delete">x</button>
+                </div>
+                <div className="task">
+                    <input type="checkbox"/>
+                    <span className="description">Walk the dog</span>
+                    <button className="delete">x</button>
+                </div>
+                <div className="task">
+                    <input type="checkbox"/>
+                    <span className="description">Call John Doe</span>
+                    <button className="delete">x</button>
+                </div>
+            </div>
+            <div className="newTask">
+                <input type="text"/>
+                <button className="create">+</button>
+            </div>
+        </div>
+    )
 }
 
 export default App;
